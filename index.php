e53c3dfadd409a922d3c524bd9c933d2e1759dcb
chmullerweb <ch.muller.web@gmail.com>
2020-06-09 16:00:21 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Charlotte Muller</title>
+    
+    <!--ICONES-->
+    
+    
+    <!-- ////   CSS   //// -->
+    <link rel="stylesheet" href="http://localhost/coursphp/CV/css/cssreset.css">
+    <link rel="stylesheet" href="http://localhost/coursphp/CV/css/grid.css">
+    <link rel="stylesheet" href="http://localhost/coursphp/CV/css/style.css">
+
+
+    <!-- ////   PATTERN   //// -->
+    <link href="https://unpkg.com/pattern.css" rel="stylesheet">
+
+    <!-- //// FONT ////-->
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,200;0,700;1,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kurale&display=swap" rel="stylesheet">
+</head>
+
+
+<body class="grid">
+    
+    
+       
+    <header class="header row1 row-sp2 col3">
+               <h1>Charlotte Muller</h1> 
+               <h2>Développeuse Web Front-End</h2>    
+
+    </header>
+    
+    <div class="row1 col6 claim">
+        <p> Vers l'infini et au-delà</p>
+    </div>
+    
+    <div class="hello row1 row-sp2 col-st10 col3">
+        
+    </div>
+    
+    <div class="loisirs row2 col4">
+        <ul>
+            <li>Randonée</li>
+            <li>Course à pied</li>
+            <li>Cuisine</li>
+            <li>Le Catch</li>
+            <li>Mon chien</li>
+            <li>Shopping d'occasion</li>
+            <li>La bière</li>
+            <li>Le Perrier menthe</li>
+        </ul>
+    </div>
+    
+    <div class="form_exp row2 row-sp2 col-st8 col5">
+        
+    </div>
+    
+    <div class="api_map row3 col7">
+        
+    </div>
+
+
+
+</body>
+</html>
