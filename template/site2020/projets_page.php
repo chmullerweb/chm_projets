08efa47fdc355a589ca70b57e898b2fdc289a563
chmullerweb <ch.muller.web@gmail.com>
2020-06-12 00:54:02 +0200

    Mise en page de la page projets_list.php (en cours)

--- template/site2020/projets_page.php
+++ template/site2020/projets_page.php
@@ -1,30 +1,62 @@
 <?php include "include/head.php"?>
 
-    
-       
-    <form class="flex search-bloc">
+<div class="title">
+    <h1>Mes projets</h1>
+</div>
+
+<section class="search bg-sapin">
+    <form class="">
+
+        <div class="search-bloc flex">
+            <img src="<?php echo $_dossier_template ?>img/zoom.png" alt="" class="icone-search">
+            <h2>Sélectionnez les projets qui vous intéresse :</h2>
+        </div>
+        <!--
+    PENSEZ A FAIRE UNE FONCTION QUI AFFICHE LES DONNEES DU TABLEAU TECHNO
+-->
+        <div class="techno-list flex">
+            <div class="flex">
+                <input type="checkbox" name="html" class="input-search">
+                <label for="html">HTML5</label>
+            </div>
+
+            <div class="flex">
+                <input type="checkbox" name="css" class="input-search">
+                <label for="css">CCS3</label>
+            </div>
+
+            <div class="flex">
+                <input type="checkbox" name="javascript" class="input-search">
+                <label for="javascript">Javascript</label>
+            </div>
+
+            <div class="flex">
+                <input type="checkbox" name="php" class="input-search">
+                <label for="php">PHP</label>
+            </div>
+        </div>
+
+       <button class="valider">Valider</button>
+    </form>
+</section>
+
+<div class="bloc-pix">
+        <img class="projet-pix" src="<?php echo $_dossier_template ?>img/Codevores1.jpg" alt="">
+</div>
+
+<div class="bloc-pix bg-vert">
+        <img class="projet-pix" src="<?php echo $_dossier_template ?>img/museum1.jpg" alt="">
+</div>
+
+<div class="bloc-pix bg-vert">
+        <img class="projet-pix" src="<?php echo $_dossier_template ?>img/museum1.jpg" alt="">
+</div>
+
+<!--Barre de navigation à creuser pour compiler JS/PHP-->
+<!--    <form class="flex search-bloc">
     <img src="<?php echo $_dossier_template ?>img/zoom.png" alt="" class="icone-search">
     <input type="text" placeholder="HTML5, Javascript, Grid..." class="input-search">
-    </form>
-    
-    <div class="hobbies">
-        <h2>Mes autres passions :</h2>
-           <ul>
-            <li>Blogging Wordpress SEO pour Bijoux Robin Paris</li>
-            <li>Bénévolat Maison Zéro Déchet</li>
-            <li>Pratique et accueil en salle de sport Swedish Fit</li>
-            <li>Lecture Bandes Dessinées</li>
-            <li>Le Perrier menthe</li>
-        </ul>
-    </div>
-    
-    <div class="profil-pix-bloc">
-        <img class="profil-pix" src="template/site2020/img/Charlotte_resize.jpg" alt="">
-    </div>
-    
-    <div class="surprise">
-       <!--Faire un bouton : une autre citation et requête avec fetch random-->
-        <button class="randomquote">Surprise !</button>
-    </div>
-    
-    <?php include "include/footer.php"?>
+    </form>-->
+
+
+<?php include "include/footer.php"?>
