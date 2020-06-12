d94d075d49e0ed21c61893a77dbf4ca10326cdef
chmullerweb <ch.muller.web@gmail.com>
2020-06-12 20:10:03 +0200

    Ajout contenu page contact

--- template/site2020/contact_page.php
+++ template/site2020/contact_page.php
@@ -1,27 +1,40 @@
 <?php include "include/head.php"?>
+ 
+    
+<div class="title">
+    <h1>Vous souhaitez me contacter ?</h1>
+</div>
 
-    
-       
-    
-    
-    <div class="hobbies">
-        <h2>Mes autres passions :</h2>
-           <ul>
-            <li>Blogging Wordpress SEO pour Bijoux Robin Paris</li>
-            <li>Bénévolat Maison Zéro Déchet</li>
-            <li>Pratique et accueil en salle de sport Swedish Fit</li>
-            <li>Lecture Bandes Dessinées</li>
-            <li>Le Perrier menthe</li>
-        </ul>
-    </div>
-    
-    <div class="profil-pix-bloc">
-        <img class="profil-pix" src="template/site2020/img/Charlotte_resize.jpg" alt="">
-    </div>
-    
-    <div class="surprise">
-       <!--Faire un bouton : une autre citation et requête avec fetch random-->
-        <button class="randomquote">Surprise !</button>
-    </div>
-    
-    <?php include "include/footer.php"?>
+    <figure class="bloc-contact phone">
+        <img class="contact-pix" src="<?php echo $_dossier_template ?>img/phone.png" alt="">
+        <figcaption class="figcaption-contact">
+            <h3>Appelez-moi</h3>
+            <p><a href="tel:0669116750">06 69 11 67 50</a></p>  <!--créer dans la bdd, l'enregistrement -->
+        </figcaption>
+    </figure>
+    <figure class="bloc-contact mail" style="margin:0">
+    <img class="contact-pix" src="<?php echo $_dossier_template ?>img/mail.png" alt="">
+        <figcaption class="figcaption-contact">
+            <h3>Ecrivez-moi</h3>
+            <p><a href="mailto:ch_muller@outlook.fr">ch_muller@outlook.fr</a></p>  <!--lister à la suite avec du php -->
+        </figcaption>
+    </figure>
+    <figure class="bloc-contact networks" style="margin:0">
+    <img class="contact-pix" src="<?php echo $_dossier_template ?>img/contacts.png" alt="">
+        <figcaption class="figcaption-contact">
+            <h3>Restons en contact</h3>
+            <div class="git flex">
+                <p>Github</p>
+               <img class="pix-networks" src="template/site2020/img/github.png" alt="">
+            </div>
+            <div class="linkedin flex">
+                <p>LinkedIn</p>
+               <img class="pix-networks" src="template/site2020/img/linkedin.png" alt="">
+            </div>
+        </figcaption>
+    </figure>
+
+   
+<section class="footer-contact">>
+<?php include "include/footer.php"?>
+</section>
