50026e3690ba2bb6fc420459d40ef829e477c6b8
chmullerweb <ch.muller.web@gmail.com>
2020-06-13 23:04:15 +0200

    Création et include de la page admin/connexion

---
+++ admin/connexion.php
@@ -0,0 +1,12 @@
+<?php 
+include "../config.php";
+include "include/head_admin.php" ?>
+
+<h1>CSS + HTML only Accordion Element</h1>
+<ul>
+  <li>
+    <input type="checkbox" checked>
+    <i></i>
+    <h2>Languages Used</h2>
+    <p>This page was written in HTML and CSS. The CSS was compiled from SASS. I used Normalize as my CSS reset and -prefix-free to save myself some headaches. I haven't quite gotten the hang of Slim for compiling into HTML, but someday I'll use it since its syntax compliments that of SASS. Regardless, this could all be done in plain HTML and CSS.</p>
+  </li>
---
+++ admin/include/head_admin.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title?></title>
+    
+    <!--ICONES-->
+    
+    
+    <!-- ////   CSS   //// -->
+    <link rel="stylesheet" href="../template/site2020/css/cssreset.css">
+    <link rel="stylesheet" href="../template/site2020/css/grid.css">
+    <link rel="stylesheet" href="../css/style_admin.css">
+
+
+    <!-- ////   PATTERN   //// -->
+    <link href="https://unpkg.com/pattern.css" rel="stylesheet">
+
+    <!-- //// FONT ////-->
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,200;0,700;1,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kurale&display=swap" rel="stylesheet">
+</head>
+
+
+<body class="grid">
+
+<header class="nav">
+        <nav>          
+            <ul class="flex">            
+               <?php include "nav_admin.php" ?>
+            </ul>
+        </nav>
+</header>
