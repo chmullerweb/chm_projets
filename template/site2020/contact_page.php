656f7afea880d897b839efc08e88f21695536abc
chmullerweb <ch.muller.web@gmail.com>
2020-06-13 22:44:29 +0200

    Responsive page contact

--- template/site2020/contact_page.php
+++ template/site2020/contact_page.php
@@ -12,14 +12,14 @@
             <p><a href="tel:0669116750">06 69 11 67 50</a></p>  <!--créer dans la bdd, l'enregistrement -->
         </figcaption>
     </figure>
-    <figure class="bloc-contact mail" style="margin:0">
+    <figure class="bloc-contact mail">
     <img class="contact-pix" src="<?php echo $_dossier_template ?>img/mail.png" alt="">
         <figcaption class="figcaption-contact">
             <h3>Ecrivez-moi</h3>
             <p><a href="mailto:ch_muller@outlook.fr">ch_muller@outlook.fr</a></p>  <!--lister à la suite avec du php -->
         </figcaption>
     </figure>
-    <figure class="bloc-contact networks" style="margin:0">
+    <figure class="bloc-contact networks">
     <img class="contact-pix" src="<?php echo $_dossier_template ?>img/contacts.png" alt="">
         <figcaption class="figcaption-contact">
             <h3>Restons en contact</h3>
