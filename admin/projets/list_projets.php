b18fb66b33dbcf4ff2c6a3b2e7132d35c0ddf120
chmullerweb <ch.muller.web@gmail.com>
2020-06-15 17:21:18 +0200

    Ajout contenu fichier list_projets.php

---
+++ admin/projets/list_projets.php
@@ -0,0 +1,19 @@
+<?php 
+include "../../config.php";
+include "../include/head_admin.php" ?>
+
+<h1>Gestion des projets</h1>
+<ul class="menu-admin">
+  <li>
+      <!-- Les titres H2 devront faire référence à la bdd -->
+    <h2>Codevores</h2>  
+    <a href="<?php echo $_url_base ?>admin/projets/form_projet.php">Modifier</a>
+    <a href="<?php echo $_url_base ?>admin/projets/delete_projet.php">Supprimer</a>
+  </li>
+  <li>
+    <h2>Museum</h2>
+    <a href="<?php echo $_url_base ?>admin/projets/form_projet.php">Modifier</a>
+    <a href="<?php echo $_url_base ?>admin/projets/delete_projet.php">Supprimer</a>
+  </li>
+  <li>
+</ul>
