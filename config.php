bf85eb939644a08fa0235b211e88837e5491c466
chmullerweb <ch.muller.web@gmail.com>
2020-06-16 11:31:16 +0200

    Connexion du projet et de la base de données mySQL dans config.php

--- config.php
+++ config.php
@@ -1,5 +1,20 @@
 <?php
+
+//J'ouvre "l'enregistrement" de la session
 session_start();
+
+//Je prépare la connexion à mon serveur et à ma base de données dans phpmyadmin
+$serveur = 'localhost';
+$utilisateur = 'root';
+$motdepasse = '';
+$nomBaseDeDonnees = "monbook_charlotte";
+
+
+//On établit la connexion en PDO
+$bdd = new PDO("mysql:host=$serveur;dbname=$nomBaseDeDonnees", $utilisateur, $motdepasse, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+
+/// Je déclare mes variables et constantes 
 
 $title = "Charlotte Muller";   //Attention la valeur de title doit être récupérée à partir du formulaire
 $titleAdmin = "Administration - monBook";
@@ -8,6 +23,7 @@ $_dossier_template = "template/site2020/"; // repertoire dans lequel j'ai mis l'
 
 $_url_base = "http://localhost/coursphp/chm_projets/";
 //  c'est l'url pour accéder à la page d'accueil de mon site.
+
 
 //ici je crée une fonction qui permet de vérifier si l'utilisateur à le droit de se connecter à l'administration. Dans ce cas, la fonction dit : si la $_SESSION("droit_connexion") est vide alors l'internaute est redirigé vers la page connexion_admin
 
