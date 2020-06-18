05c2518323ffb26c79f959b8346f3f16270b49a7
chmullerweb <ch.muller.web@gmail.com>
2020-06-18 12:40:52 +0200

    Liste projets x mysql dans projets_page.php

--- template/site2020/projets_page.php
+++ template/site2020/projets_page.php
@@ -8,6 +8,11 @@ $query = $bdd -> prepare("SELECT * from technos where iduu = :iduu");
 $query -> execute([":iduu" => "TEXT_TECHNO"]);
 $list_techno = $query ->  fetchAll(PDO::FETCH_ASSOC);
 
+// on importe le contenu de la table projets de ma bdd
+$query = $bdd -> prepare("SELECT * from projets where iduu = :iduu");
+$query -> execute([":iduu" => "TEXT_PROJET"]);
+$list_projet = $query ->  fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 
 
@@ -16,16 +21,13 @@ $list_techno = $query ->  fetchAll(PDO::FETCH_ASSOC);
 </div>
 
 <section class="search bg-sapin">
-    <form class="">
 
         <div class="search-bloc flex">
             <img src="<?php echo $_dossier_template ?>img/zoom.png" alt="" class="icone-search">
             <h2>Sélectionnez les projets qui vous intéresse :</h2>
         </div>
-        <!--
-    PENSEZ A FAIRE UNE FONCTION QUI AFFICHE LES DONNEES DU TABLEAU TECHNO
--->
-
+        <!--    Pour chaque techno de ma table techno, je l'affiche -->
+        <form action="projets_page.php" method="post">
         <div class="techno-list flex">
         <?php foreach($list_techno as $key => $techno){?>
             <div class="flex">
@@ -33,37 +35,25 @@ $list_techno = $query ->  fetchAll(PDO::FETCH_ASSOC);
                 <label for="<?php echo $techno["nomtechno"]?>"><?php echo $techno["nomtechno"]?></label>
             </div>
 <?php }; ?>
-            
-        <button class="valider">Valider</button>
+        <button type="submit" class="valider" style="margin-top: 3rem">Valider</button>
     </form>
 </section>
 
 <section class="grid-projet">
+        <!--    Pour chaque projet de ma table projets, je l'affiche -->
+    <?php foreach($list_projet as $key => $projet){?>
+    
     <figure class="bloc-pix-projet" style="margin:0">
-        <img class="projet-pix" src="<?php echo $_dossier_template ?>img/Codevores1.jpg" alt="">
+        <img class="projet-pix" src="<?php echo $_dossier_template . $projet["img_main"]?>" alt="">
         <figcaption>
-            <h3>Codevores</h3>
+            <h3><?php echo $projet["titre"]?></h3>
             <p>Langages : </p>  <!--lister à la suite avec du php -->
             <a href="<?php echo $_url_base?>projet.php"><img src="<?php echo $_dossier_template ?>img/zoom.png" alt="" style="width:2rem"></a>
         </figcaption>
     </figure>
-    <figure class="bloc-pix-projet" style="margin:0">
-    <img class="projet-pix" src="<?php echo $_dossier_template ?>img/museum1.jpg" alt="">
-        <figcaption>
-            <h3>Museum</h3>
-            <p>Langages : </p>  <!--lister à la suite avec du php -->
-            <a href="<?php echo $_url_base?>projet.php"><img src="<?php echo $_dossier_template ?>img/zoom.png" alt="" style="width:2rem"></a>
-        </figcaption>
-    </figure>
-
-
+    <?php };?>
 </section>
 
-<!--Barre de navigation à creuser pour compiler JS/PHP-->
-<!--    <form class="flex search-bloc">
-    <img src="<?php echo $_dossier_template ?>img/zoom.png" alt="" class="icone-search">
-    <input type="text" placeholder="HTML5, Javascript, Grid..." class="input-search">
-    </form>-->
 
 
 <?php include "include/footer.php"?>
